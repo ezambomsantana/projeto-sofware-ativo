d1db5b1c75c288810685262ad051f39e09c84ddb
Eduardo Santana <ezambomsantana@gmail.com>
2025-09-30 21:40:39 -0300

    fix paths

--- src/main/java/br/insper/cotacao/stocks/service/MovimentacaoService.java
+++ src/main/java/br/insper/cotacao/stocks/service/MovimentacaoService.java
@@ -1,5 +1,6 @@
 package br.insper.cotacao.stocks.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,20 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+
 @Service
 public class MovimentacaoService {
+
+
+    @Value("${movimentacao.url}")
+    private String movimentacaoUrl;
 
     public List<Movimentacao> getMovimetacoes() {
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<List<Movimentacao>> response =
                 restTemplate.exchange(
-                        "http://localhost:5000/movimentacoes",
+                        movimentacaoUrl + "/movimentacoes",
                         HttpMethod.GET,
                         null,
                         new ParameterizedTypeReference<>() {}
